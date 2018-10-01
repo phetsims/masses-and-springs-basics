6c342889c43ed81e5bf8517e477627adc651b3da
denz1994 <denz1994@gmail.com>
2018-10-01 13:24:30 -0400

    Building masses-and-springs-basics to import from masses-and-springs.

--- js/masses-and-springs-basics-main.js
+++ js/masses-and-springs-basics-main.js
@@ -9,27 +9,37 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var EnergyScreen = require( 'MASSES_AND_SPRINGS/energy/EnergyScreen' );
+  var IntroScreen = require( 'MASSES_AND_SPRINGS/intro/IntroScreen' );
+  var LabScreen = require( 'MASSES_AND_SPRINGS/lab/LabScreen' );
   var Sim = require( 'JOIST/Sim' );
   var SimLauncher = require( 'JOIST/SimLauncher' );
-  var MassesAndSpringsBasicsScreen = require( 'MASSES_AND_SPRINGS_BASICS/masses-and-springs-basics/MassesAndSpringsBasicsScreen' );
+  var Tandem = require( 'TANDEM/Tandem' );
+  var VectorsScreen = require( 'MASSES_AND_SPRINGS/vectors/VectorsScreen' );
 
   // strings
   var massesAndSpringsBasicsTitleString = require( 'string!MASSES_AND_SPRINGS_BASICS/masses-and-springs-basics.title' );
 
+  // constants
+  var tandem = Tandem.rootTandem;
+
   var simOptions = {
     credits: {
-      //TODO fill in proper credits, all of these fields are optional, see joist.AboutDialog
-      leadDesign: '',
-      softwareDevelopment: '',
-      team: '',
-      qualityAssurance: '',
-      graphicArts: '',
-      thanks: ''
+      leadDesign: 'Amy Rouinfar, Mike Dubson',
+      softwareDevelopment: 'Denzell Barnett, Matt Pennington',
+      team: 'Wendy Adams, Ariel Paul, Kathy Perkins',
+      qualityAssurance:
+        'Steele Dalton, Bryce Griebenow, Ethan Johnson, Megan Lai, Liam Mulhall, Arnab Purkayastha, Benjamin Roberts, Jacob Romero, Clara Wilson, Kathryn Woessner'
     }
   };
 
   SimLauncher.launch( function() {
-    var sim = new Sim( massesAndSpringsBasicsTitleString, [ new MassesAndSpringsBasicsScreen() ], simOptions );
+    var sim = new Sim( massesAndSpringsBasicsTitleString, [
+      new IntroScreen( tandem.createTandem( 'introScreen' ), { basicsVersion: true } ),
+      new VectorsScreen( tandem.createTandem( 'vectorsScreen' ), { basicsVersion: true } ),
+      new EnergyScreen( tandem.createTandem( 'energyScreen' ), { basicsVersion: true } ),
+      new LabScreen( tandem.createTandem( 'labScreen' ), { basicsVersion: true } )
+    ], simOptions );
     sim.start();
   } );
 } );
