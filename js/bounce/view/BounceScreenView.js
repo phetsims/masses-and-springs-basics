6fedadc4ea8a2741080d69bd7a3da12cf7456927
denz1994 <denz1994@gmail.com>
2019-01-16 16:42:54 -0500

    Convert BounceScreenView.js to ES6.

--- js/bounce/view/BounceScreenView.js
+++ js/bounce/view/BounceScreenView.js
@@ -5,115 +5,115 @@
  *
  * @author Denzell Barnett (PhET Interactive Simulations)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var DerivedProperty = require( 'AXON/DerivedProperty' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var LineOptionsNode = require( 'MASSES_AND_SPRINGS_BASICS/common/view/LineOptionsNode' );
-  var massesAndSpringsBasics = require( 'MASSES_AND_SPRINGS_BASICS/massesAndSpringsBasics' );
-  var MassesAndSpringsColorProfile = require( 'MASSES_AND_SPRINGS/common/view/MassesAndSpringsColorProfile' );
-  var MassesAndSpringsConstants = require( 'MASSES_AND_SPRINGS/common/MassesAndSpringsConstants' );
-  var ReferenceLineNode = require( 'MASSES_AND_SPRINGS/common/view/ReferenceLineNode' );
-  var Shelf = require( 'MASSES_AND_SPRINGS/common/view/Shelf' );
-  var TwoSpringScreenView = require( 'MASSES_AND_SPRINGS/common/view/TwoSpringScreenView' );
-  var VBox = require( 'SCENERY/nodes/VBox' );
-  var Vector2 = require( 'DOT/Vector2' );
+  const DerivedProperty = require( 'AXON/DerivedProperty' );
+  const inherit = require( 'PHET_CORE/inherit' );
+  const LineOptionsNode = require( 'MASSES_AND_SPRINGS_BASICS/common/view/LineOptionsNode' );
+  const massesAndSpringsBasics = require( 'MASSES_AND_SPRINGS_BASICS/massesAndSpringsBasics' );
+  const MassesAndSpringsColorProfile = require( 'MASSES_AND_SPRINGS/common/view/MassesAndSpringsColorProfile' );
+  const MassesAndSpringsConstants = require( 'MASSES_AND_SPRINGS/common/MassesAndSpringsConstants' );
+  const ReferenceLineNode = require( 'MASSES_AND_SPRINGS/common/view/ReferenceLineNode' );
+  const Shelf = require( 'MASSES_AND_SPRINGS/common/view/Shelf' );
+  const TwoSpringScreenView = require( 'MASSES_AND_SPRINGS/common/view/TwoSpringScreenView' );
+  const VBox = require( 'SCENERY/nodes/VBox' );
+  const Vector2 = require( 'DOT/Vector2' );
 
-  /**
-   * @param {VectorsModel} model
-   * @param {Tandem} tandem
-   * @constructor
-   */
-  function BounceScreenView( model, tandem ) {
+  class BounceScreenView extends TwoSpringScreenView {
+    /**
+     * @param {VectorsModel} model
+     * @param {Tandem} tandem
+     */
+    constructor( model, tandem ) {
 
-    // Calls common two spring view
-    TwoSpringScreenView.call( this, model, tandem );
-    var self = this;
+      super( model, tandem );
 
-    // Equilibrium of mass is dependent on the mass being attached and the visibility of the equilibrium line.
-    var firstMassEquilibriumVisibilityProperty = new DerivedProperty( [ model.equilibriumPositionVisibleProperty, model.firstSpring.massAttachedProperty ],
-      function( equilibriumPositionVisible, massAttached ) {
-        return !!massAttached && equilibriumPositionVisible;
+      // Equilibrium of mass is dependent on the mass being attached and the visibility of the equilibrium line.
+      let firstMassEquilibriumVisibilityProperty = new DerivedProperty( [ model.equilibriumPositionVisibleProperty, model.firstSpring.massAttachedProperty ],
+        function( equilibriumPositionVisible, massAttached ) {
+          return !!massAttached && equilibriumPositionVisible;
+        } );
+      let secondMassEquilibriumVisibilityProperty = new DerivedProperty( [ model.equilibriumPositionVisibleProperty, model.secondSpring.massAttachedProperty ],
+        function( equilibriumPositionVisible, massAttached ) {
+          return !!massAttached && equilibriumPositionVisible;
+        } );
+
+      // Initializes equilibrium line for first spring
+      const firstMassEquilibriumLineNode = new ReferenceLineNode(
+        this.modelViewTransform,
+        model.firstSpring,
+        model.firstSpring.equilibriumYPositionProperty,
+        firstMassEquilibriumVisibilityProperty, {
+          stroke: MassesAndSpringsColorProfile.restingPositionProperty
+        }
+      );
+
+      // Initializes equilibrium line for second spring
+      const secondMassEquilibriumLineNode = new ReferenceLineNode(
+        this.modelViewTransform,
+        model.secondSpring,
+        model.secondSpring.equilibriumYPositionProperty,
+        secondMassEquilibriumVisibilityProperty, {
+          stroke: MassesAndSpringsColorProfile.restingPositionProperty
+        }
+      );
+
+      // Adding system controls to scene graph
+      this.addChild( this.springSystemControlsNode );
+
+      // Reference lines from indicator visibility box
+      this.addChild( this.firstNaturalLengthLineNode );
+      this.addChild( this.secondNaturalLengthLineNode );
+      this.addChild( firstMassEquilibriumLineNode );
+      this.addChild( secondMassEquilibriumLineNode );
+      this.addChild( this.movableLineNode );
+      this.addChild( this.massLayer );
+      this.addChild( this.toolsLayer );
+
+      // Panel that will display all the toggleable options.
+      const optionsPanel = this.createOptionsPanel(
+        new LineOptionsNode( model, tandem ),
+        this.rightPanelAlignGroup,
+        tandem
+      );
+
+      // Contains all of the options for the reference lines, gravity, damping, and toolbox
+      const rightPanelsVBox = new VBox( {
+        children: [ optionsPanel, this.toolboxPanel ],
+        spacing: this.spacing * 0.9
       } );
-    var secondMassEquilibriumVisibilityProperty = new DerivedProperty( [ model.equilibriumPositionVisibleProperty, model.secondSpring.massAttachedProperty ],
-      function( equilibriumPositionVisible, massAttached ) {
-        return !!massAttached && equilibriumPositionVisible;
+      this.addChild( rightPanelsVBox );
+      rightPanelsVBox.moveToBack();
+
+      this.visibleBoundsProperty.link( () => {
+        rightPanelsVBox.rightTop = new Vector2( this.panelRightSpacing, this.spacing );
       } );
 
-    // Initializes equilibrium line for first spring
-    var firstMassEquilibriumLineNode = new ReferenceLineNode(
-      this.modelViewTransform,
-      model.firstSpring,
-      model.firstSpring.equilibriumYPositionProperty,
-      firstMassEquilibriumVisibilityProperty, {
-        stroke: MassesAndSpringsColorProfile.restingPositionProperty
-      }
-    );
+      // Move this plane to the back of the scene graph
+      this.backgroundDragPlane.moveToBack();
 
-    // Initializes equilibrium line for second spring
-    var secondMassEquilibriumLineNode = new ReferenceLineNode(
-      this.modelViewTransform,
-      model.secondSpring,
-      model.secondSpring.equilibriumYPositionProperty,
-      secondMassEquilibriumVisibilityProperty, {
-        stroke: MassesAndSpringsColorProfile.restingPositionProperty
-      }
-    );
+      // Shelves used for masses
+      const labeledMassesShelf = new Shelf( tandem, {
+        rectHeight: 7,
+        rectWidth: 185,
+        left: this.layoutBounds.left + this.spacing,
+        rectY: this.modelViewTransform.modelToViewY( MassesAndSpringsConstants.FLOOR_Y ) - this.shelf.rectHeight
+      } );
+      this.addChild( labeledMassesShelf );
+      labeledMassesShelf.moveToBack();
 
-    // Adding system controls to scene graph
-    this.addChild( this.springSystemControlsNode );
-
-    // Reference lines from indicator visibility box
-    this.addChild( this.firstNaturalLengthLineNode );
-    this.addChild( this.secondNaturalLengthLineNode );
-    this.addChild( firstMassEquilibriumLineNode );
-    this.addChild( secondMassEquilibriumLineNode );
-    this.addChild( this.movableLineNode );
-    this.addChild( this.massLayer );
-    this.addChild( this.toolsLayer );
-
-    // Panel that will display all the toggleable options.
-    var optionsPanel = this.createOptionsPanel(
-      new LineOptionsNode( model, tandem ),
-      this.rightPanelAlignGroup,
-      tandem
-    );
-
-    // Contains all of the options for the reference lines, gravity, damping, and toolbox
-    var rightPanelsVBox = new VBox( { children: [ optionsPanel, self.toolboxPanel ], spacing: this.spacing * 0.9 } );
-    this.addChild( rightPanelsVBox );
-    rightPanelsVBox.moveToBack();
-
-    this.visibleBoundsProperty.link( function() {
-      rightPanelsVBox.rightTop = new Vector2( self.panelRightSpacing, self.spacing);
-    } );
-
-    // Move this plane to the back of the scene graph
-    this.backgroundDragPlane.moveToBack();
-
-    // Shelves used for masses
-    var labeledMassesShelf = new Shelf( tandem, {
-      rectHeight: 7,
-      rectWidth: 185,
-      left: this.layoutBounds.left + this.spacing,
-      rectY: this.modelViewTransform.modelToViewY( MassesAndSpringsConstants.FLOOR_Y ) - this.shelf.rectHeight
-    } );
-    this.addChild(labeledMassesShelf);
-    labeledMassesShelf.moveToBack();
-
-    var mysteryMassesShelf = new Shelf( tandem, {
-      rectHeight: 7,
-      rectWidth: 120,
-      left: labeledMassesShelf.right + this.spacing * 2,
-      rectY: this.modelViewTransform.modelToViewY( MassesAndSpringsConstants.FLOOR_Y ) - this.shelf.rectHeight
-    } );
-    this.addChild(mysteryMassesShelf);
-    mysteryMassesShelf.moveToBack();
+      const mysteryMassesShelf = new Shelf( tandem, {
+        rectHeight: 7,
+        rectWidth: 120,
+        left: labeledMassesShelf.right + this.spacing * 2,
+        rectY: this.modelViewTransform.modelToViewY( MassesAndSpringsConstants.FLOOR_Y ) - this.shelf.rectHeight
+      } );
+      this.addChild( mysteryMassesShelf );
+      mysteryMassesShelf.moveToBack();
+    }
   }
 
-  massesAndSpringsBasics.register( 'BounceScreenView', BounceScreenView );
-
-  return inherit( TwoSpringScreenView, BounceScreenView );
+  return massesAndSpringsBasics.register( 'BounceScreenView', BounceScreenView );
 } );
