a338bcba46a9a390324aad1e05d7fac0f7155b62
denz1994 <denz1994@gmail.com>
2019-01-16 23:31:20 -0500

    Convert StretchScreen.js to ES6.

--- js/masses-and-springs-basics-main.js
+++ js/masses-and-springs-basics-main.js
@@ -5,26 +5,26 @@
  *
  * @author Denzell Barnett (PhET Interactive Simulations)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var BounceScreen = require( 'MASSES_AND_SPRINGS_BASICS/bounce/BounceScreen' );
-  var LabScreen = require( 'MASSES_AND_SPRINGS_BASICS/lab/LabScreen' );
-  var MassesAndSpringsColorProfile = require( 'MASSES_AND_SPRINGS/common/view/MassesAndSpringsColorProfile' );
-  var Sim = require( 'JOIST/Sim' );
-  var SimLauncher = require( 'JOIST/SimLauncher' );
-  var StretchScreen = require( 'MASSES_AND_SPRINGS_BASICS/stretch/StretchScreen' );
-  var Tandem = require( 'TANDEM/Tandem' );
+  const BounceScreen = require( 'MASSES_AND_SPRINGS_BASICS/bounce/BounceScreen' );
+  const LabScreen = require( 'MASSES_AND_SPRINGS_BASICS/lab/LabScreen' );
+  const MassesAndSpringsColorProfile = require( 'MASSES_AND_SPRINGS/common/view/MassesAndSpringsColorProfile' );
+  const Sim = require( 'JOIST/Sim' );
+  const SimLauncher = require( 'JOIST/SimLauncher' );
+  const StretchScreen = require( 'MASSES_AND_SPRINGS_BASICS/stretch/StretchScreen' );
+  const Tandem = require( 'TANDEM/Tandem' );
 
   // strings
-  var massesAndSpringsBasicsTitleString = require( 'string!MASSES_AND_SPRINGS_BASICS/masses-and-springs-basics.title' );
+  const massesAndSpringsBasicsTitleString = require( 'string!MASSES_AND_SPRINGS_BASICS/masses-and-springs-basics.title' );
 
   // constants
-  var tandem = Tandem.rootTandem;
+  const tandem = Tandem.rootTandem;
 
   // Todo: Correct credits.
-  var simOptions = {
+  const simOptions = {
     credits: {
       leadDesign: 'Amy Rouinfar, Mike Dubson',
       softwareDevelopment: 'Denzell Barnett',
@@ -36,7 +36,7 @@ define( function( require ) {
   MassesAndSpringsColorProfile.profileNameProperty.set( 'basics' );
 
   SimLauncher.launch( function() {
-    var sim = new Sim( massesAndSpringsBasicsTitleString, [
+    const sim = new Sim( massesAndSpringsBasicsTitleString, [
       new StretchScreen( tandem.createTandem( 'stretchScreen' ) ),
       new BounceScreen( tandem.createTandem( 'bounceScreen' ) ),
       new LabScreen( tandem.createTandem( 'labScreen' ) )
