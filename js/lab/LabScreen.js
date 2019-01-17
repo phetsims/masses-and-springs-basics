436bce3be4d55910abb32109c3624edbacb3d6b7
denz1994 <denz1994@gmail.com>
2019-01-16 19:23:20 -0500

    Convert LabScreen.js to ES6.

--- js/lab/LabScreen.js
+++ js/lab/LabScreen.js
@@ -5,51 +5,48 @@
  *
  * @author Denzell Barnett (PhET Interactive Simulations)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var Image = require( 'SCENERY/nodes/Image' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var LabModel = require( 'MASSES_AND_SPRINGS/lab/model/LabModel' );
-  var LabScreenView = require( 'MASSES_AND_SPRINGS_BASICS/lab/view/LabScreenView' );
-  var massesAndSpringsBasics = require( 'MASSES_AND_SPRINGS_BASICS/massesAndSpringsBasics' );
-  var MassesAndSpringsColorProfile = require( 'MASSES_AND_SPRINGS/common/view/MassesAndSpringsColorProfile' );
-  var Screen = require( 'JOIST/Screen' );
+  const Image = require( 'SCENERY/nodes/Image' );
+  const LabModel = require( 'MASSES_AND_SPRINGS/lab/model/LabModel' );
+  const LabScreenView = require( 'MASSES_AND_SPRINGS_BASICS/lab/view/LabScreenView' );
+  const massesAndSpringsBasics = require( 'MASSES_AND_SPRINGS_BASICS/massesAndSpringsBasics' );
+  const MassesAndSpringsColorProfile = require( 'MASSES_AND_SPRINGS/common/view/MassesAndSpringsColorProfile' );
+  const Screen = require( 'JOIST/Screen' );
 
   // strings
-  var screenLabString = require( 'string!MASSES_AND_SPRINGS_BASICS/screen.lab' );
+  const screenLabString = require( 'string!MASSES_AND_SPRINGS_BASICS/screen.lab' );
 
   // image
-  var labHomeScreenImage = require( 'image!MASSES_AND_SPRINGS_BASICS/lab_screen_icon.png' );
+  const labHomeScreenImage = require( 'image!MASSES_AND_SPRINGS_BASICS/lab_screen_icon.png' );
 
-  /**
-   * @param {Tandem} tandem
-   * @param {object} options
-   *
-   * @constructor
-   */
-  function LabScreen( tandem, options ) {
+  class LabScreen extends Screen {
 
-    options = _.extend( {
-      basicsVersion: true,
-      name: screenLabString,
-      backgroundColorProperty: MassesAndSpringsColorProfile.backgroundProperty,
-      homeScreenIcon: new Image( labHomeScreenImage ),
-      tandem: tandem
-    }, options );
+    /**
+     * @param {Tandem} tandem
+     * @param {object} options
+     *
+     * @constructor
+     */
+    constructor( tandem, options ) {
 
-    Screen.call( this,
-      function() {
-        return new LabModel( tandem.createTandem( 'model' ), options );
-      },
-      function( model ) {
-        return new LabScreenView( model, tandem );
-      },
-      options
-    );
+      options = _.extend( {
+        basicsVersion: true,
+        name: screenLabString,
+        backgroundColorProperty: MassesAndSpringsColorProfile.backgroundProperty,
+        homeScreenIcon: new Image( labHomeScreenImage ),
+        tandem: tandem
+      }, options );
+
+      super(
+        () => new LabModel( tandem.createTandem( 'model' ), options ),
+        model => { return new LabScreenView( model, tandem ); },
+        options
+      );
+    }
   }
 
-  massesAndSpringsBasics.register( 'LabScreen', LabScreen );
-  return inherit( Screen, LabScreen );
+  return massesAndSpringsBasics.register( 'LabScreen', LabScreen );
 } );
