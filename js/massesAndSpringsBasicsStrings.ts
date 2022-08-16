be71a178646a2b612a488a250ec779a872fbe69d
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:33 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/massesAndSpringsBasicsStrings.ts
+++ js/massesAndSpringsBasicsStrings.ts
@@ -5,20 +5,28 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import massesAndSpringsBasics from './massesAndSpringsBasics.js';
 
 type StringsType = {
   'centerOfOscillation': string;
+  'centerOfOscillationProperty': TReadOnlyProperty<string>;
   'masses-and-springs-basics': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'stretch': string;
+    'stretchProperty': TReadOnlyProperty<string>;
     'bounce': string;
+    'bounceProperty': TReadOnlyProperty<string>;
     'lab': string;
+    'labProperty': TReadOnlyProperty<string>;
   };
   'restingPosition': string;
+  'restingPositionProperty': TReadOnlyProperty<string>;
   'unstretchedLength': string;
+  'unstretchedLengthProperty': TReadOnlyProperty<string>;
 };
 
 const massesAndSpringsBasicsStrings = getStringModule( 'MASSES_AND_SPRINGS_BASICS' ) as StringsType;
